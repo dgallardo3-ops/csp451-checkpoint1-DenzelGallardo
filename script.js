f8a8730f45fe347d94131ffae3a19271bfe199e0
dgallardo3-ops <denzel.gallardo04@gmail.com>
2026-01-12 19:14:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("CSP451 CheckPoint 1 loaded successfully!");
+
+const btn = document.getElementById("testBtn");
+btn.addEventListener("click", () => {
+  console.log("Button clicked!");
+  alert("Button works — check the console!");
+});
